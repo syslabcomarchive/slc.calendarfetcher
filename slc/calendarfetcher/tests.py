30f3202ee458ce22a7c50158756a923d238fdad2
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-11-24 15:12:00 +0000

    Add tests

--- slc/calendarfetcher/tests.py
+++ slc/calendarfetcher/tests.py
@@ -2,6 +2,9 @@ import unittest
 
 #from zope.testing import doctestunit
 #from zope.component import testing
+from zope import component
+from zope.interface import alsoProvides
+from zope.annotation.interfaces import IAnnotations
 from Testing import ZopeTestCase as ztc
 
 from Products.Five import fiveconfigure
@@ -10,6 +13,7 @@ from Products.PloneTestCase.layer import PloneSite
 ptc.setupPloneSite()
 
 import slc.calendarfetcher
+from p4a.calendar.interfaces import ICalendarEnhanced
 
 
 class TestCase(ptc.PloneTestCase):
@@ -27,29 +31,48 @@ class TestCase(ptc.PloneTestCase):
             pass
 
 
+class TestFetcher(TestCase):
+
+    def afterSetUp(self):
+        """ Create a Seminar object, and call the relevant event to enable the
+            auto-creation of the sub-objects ('speakers', 'speech venues').
+        """
+        self.loginAsPortalOwner()
+        portal = self.portal
+        portal.invokeFactory('Folder', 'calendar', title="Calendar")
+        calendar = getattr(portal, 'calendar')
+        annotations = IAnnotations(calendar)
+        annotations['slc.calendarfetcher-urls'] = [
+            'http://www.google.com/calendar/ical/german__en%40holiday.calendar.google.com/public/basic.ics',
+            'http://www.google.com/calendar/ical/sa__en%40holiday.calendar.google.com/public/basic.ics',
+            ]
+        alsoProvides(calendar, ICalendarEnhanced)
+
+
+    def test_calendarfetcher(self):
+        """ Tests:
+            slc.calendarfetcher.browser.calendarfetcher.CalendarFetcherUtils 
+        """
+        portal = self.portal
+        calendar = getattr(portal, 'calendar')
+        request = self.folder.REQUEST
+        view = component.queryMultiAdapter(
+                                    (calendar, request), 
+                                    name='calendarfetcher_utils', 
+                                    default=None
+                                    )
+
+        # Check that subflolders are created.
+        view.fetch_calendars()
+        self.assertEquals(len(calendar.objectValues()) !=  0, True)
+
+
+
 def test_suite():
-    return unittest.TestSuite([
-
-        # Unit tests
-        #doctestunit.DocFileSuite(
-        #    'README.txt', package='slc.calendarfetcher',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
-
-        #doctestunit.DocTestSuite(
-        #    module='slc.calendarfetcher.mymodule',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
-
-
-        # Integration tests that use PloneTestCase
-        #ztc.ZopeDocFileSuite(
-        #    'README.txt', package='slc.calendarfetcher',
-        #    test_class=TestCase),
-
-        #ztc.FunctionalDocFileSuite(
-        #    'browser.txt', package='slc.calendarfetcher',
-        #    test_class=TestCase),
-
-        ])
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(TestFetcher))
+    return suite
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')
